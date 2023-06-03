4ec3a7c0eddd88b220837eb1d580051c68be4ad1
jayaprakashajaishee <jayaprakashajaishee@gmail.com>
2023-06-03 22:12:07 +0530

    mongoose, db connection

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,21 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
 const port = process.env.PORT;
+const mongooseURI = process.env.MONGO_URI;
+
+mongoose.connect(mongooseURI);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
+});
+
+database.once("connected", () => {
+  console.log("Database Connected");
+});
 
 //log route middleware
 app.use((req, res, next) => {
