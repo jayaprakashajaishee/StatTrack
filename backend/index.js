de208550e8c0056d8f2a8246d3c8f692eeea9386
jayaprakashajaishee <jayaprakashajaishee@gmail.com>
2023-06-03 23:32:58 +0530

    use User Router

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const userRouter = require("./routes/userRouter");
 const app = express();
 
 const port = process.env.PORT;
@@ -16,6 +17,8 @@ database.on("error", (error) => {
 database.once("connected", () => {
   console.log("Database Connected");
 });
+
+app.use("/user", userRouter);
 
 //log route middleware
 app.use((req, res, next) => {
