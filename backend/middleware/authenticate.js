dcca7b72347c996e1c17c1d1468e181a52968ec2
jayaprakashajaishee <jayaprakashajaishee@gmail.com>
2023-08-27 19:59:16 +0530

    authenticate middleware

--- backend/middleware/authenticate.js
+++ backend/middleware/authenticate.js
@@ -2,11 +2,12 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 const authenticate = (req, res, next) => {
-  const authToken = req.headers.authorization?.split(" ")[1];
-  if (!authToken)
-    return res.json({ status: 401, message: "NO-TOKEN" }).status(401);
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) return res.sendStatus(401);
+
   jwt.verify(authToken, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.send({ status: 400, message: err.message });
+    if (err) return res.sendStatus(403);
 
     req.user = user;
     next();
